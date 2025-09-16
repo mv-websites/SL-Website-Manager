d8fb389c3e416d61c817545123fee76be3069bc2
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2025-09-16 11:43:44 +0000

    Created function that bulk updates all parent products when supplier discount is edited

--- pages/Suppliers/jsobjects/JSObject1/JSObject1.js
+++
@@ -1,6 +0,0 @@
-export default {
-	async updateProductPricing () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
-	}
-}
---
+++ pages/Suppliers/jsobjects/Supplier_Functions/Supplier_Functions.js
@@ -0,0 +1,38 @@
+export default {
+	calculatePrice (listPrice, supplierDiscount, markup) {
+		supplierDiscount = supplierDiscount/100;
+		markup = markup/100;
+		const priceAfterSupplierDiscount = (listPrice * (1-supplierDiscount)) || 1;
+		return `${priceAfterSupplierDiscount * (1+markup)}`
+	},
+	async updateProductPricing () {
+		const count = await Supplier_Product_Count.data[0].row_count
+		const loopCount = Math.ceil(count/100);
+		
+		for (let i = 0; i < loopCount; i++) {
+			const products = await Get_Supplier_Products.run({ offsetVar: 0 * 100 });
+			const mappedProducts = products.map((val) => {
+				const newPrice = Supplier_Functions.calculatePrice(val.aq_list_price, val.supplier_discount, val.product_markup)
+				return {
+					"id": val.product_id,
+					"regular_price": newPrice
+				}
+			})
+			const response = await Batch_Update_Products.run({
+				body: {
+					"update": mappedProducts
+				}
+			});
+			console.log(response)
+		}
+		// console.log(testVar)
+		// Get all products where the supplier ID = 123
+		// LOOP1: for each 100 products
+			// map products to { id: 123, regular_price: 123 }
+			// Post to website the mapped products
+		
+			// LOOP2: for each product that has a variation
+				// map variations to { id: 123, regular_price: 123 }
+				// Post to website the mapped variations
+	}
+}
