ee23271bc43fb7d58fc5542f24e98b07fabe0201
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2025-09-25 12:22:37 +0000

    Added siclops Ref to Users

--- pages/Customers/jsobjects/JSObject1/JSObject1.js
+++ pages/Customers/jsobjects/JSObject1/JSObject1.js
@@ -3,6 +3,10 @@ export default {
     const item = meta_data.find(d => d.key === "reactive_jobs");
     return item ? item : { "key": "reactive_jobs", "value": "" };
 	},
+	formatClientRefFromMeta (meta_data) {
+    const item = meta_data.find(d => d.key === "client_ref");
+    return item ? item : { "key": "client_ref", "value": "" };
+	},
 	async formattedCustomerData () {
 		const rawData = await Get_Customers.run({"search": Table1.searchText, "page": Table1.pageNo});
 		
@@ -13,7 +17,9 @@ export default {
 				email: customer.email,
 				username: customer.username,
 				powerBI: JSObject1.formatPowerBIFromMeta(customer.meta_data).value,
-				powerBIObject: JSObject1.formatPowerBIFromMeta(customer.meta_data)
+				powerBIObject: JSObject1.formatPowerBIFromMeta(customer.meta_data),
+				ClientRef: JSObject1.formatClientRefFromMeta(customer.meta_data).value,
+				ClientRefObject: JSObject1.formatClientRefFromMeta(customer.meta_data)
 			}
 		})
 
