5695c6a05d929e2ef73664c89ce06a64d2288d79
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2025-09-29 08:44:52 +0000

    Created Asset Replacement Manager page

---
+++ pages/BER Assets/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,33 @@
+export default {
+	formatPowerBIFromMeta (meta_data) {
+    const item = meta_data.find(d => d.key === "reactive_jobs");
+    return item ? item : { "key": "reactive_jobs", "value": "" };
+	},
+	formatClientGroupFromMeta (meta_data) {
+    const item = meta_data.find(d => d.key === "client_group");
+    return item ? item : { "key": "client_group", "value": "" };
+	},
+	formatClientRefFromMeta (meta_data) {
+    const item = meta_data.find(d => d.key === "client_ref");
+    return item ? item : { "key": "client_ref", "value": "" };
+	},
+	async formattedCustomerData () {
+		const rawData = await Get_Customers.run({"search": Table1.searchText, "page": Table1.pageNo});
+		
+		const formattedData = rawData.map((customer) => {
+			return {
+				id: customer.id,
+				name: customer.first_name + " " + customer.last_name,
+				email: customer.email,
+				username: customer.username,
+				powerBI: JSObject1.formatPowerBIFromMeta(customer.meta_data).value,
+				powerBIObject: JSObject1.formatPowerBIFromMeta(customer.meta_data),
+				ClientGroup: JSObject1.formatClientGroupFromMeta(customer.meta_data).value,
+				ClientGroupObject: JSObject1.formatClientGroupFromMeta(customer.meta_data),
+				ClientRefObject: JSObject1.formatClientRefFromMeta(customer.meta_data)
+			}
+		})
+
+		return formattedData;
+	}
+}
