1c796bb8e90a1b675b6249347cf49746cab6053f
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2025-10-06 10:08:19 +0000

    Added queries to view if customer has viewed product and the viewed product time and by who

--- pages/BER Assets/jsobjects/Requests/Requests.js
+++ pages/BER Assets/jsobjects/Requests/Requests.js
@@ -3,6 +3,7 @@ export default {
 	removeValue("selectedReplacementProducts");
 	showModal(LoadingModal.name)
 	const sqlData = await Asset_Repl_Stat.run({ eq_code: appsmith.store.eq_code });
+	const replacementStatus = await Get_Asset_replacement_Status.run({eq_code: appsmith.store.eq_code})
 
 		// Build the full products object
 		const selectedProducts = {};
@@ -24,9 +25,14 @@ export default {
 
 		// Store the entire object **once**, outside the loop
 		storeValue("selectedReplacementProducts", selectedProducts);
+		storeValue("replacementStatus", replacementStatus[0])
 		closeModal(LoadingModal.name)
 
 		return sqlData;
+	},
+	async updateCustomerLinkDetails () {
+		const replacementStatus = await Get_Asset_replacement_Status.run({eq_code: appsmith.store.eq_code})
+		storeValue("replacementStatus", replacementStatus[0])
 	},
 	removeValues(){
 		removeValue("selectedReplacementProducts")
