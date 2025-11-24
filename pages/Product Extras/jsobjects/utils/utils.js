8610d2eb5e95cb6670d5c7cf71663ef687d5b75d
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2025-11-24 13:11:35 +0000

    Completed modal that shows associated products to a Label/Product addon

---
+++ pages/Product Extras/jsobjects/utils/utils.js
@@ -0,0 +1,20 @@
+export default {
+	async GetLabels () {
+		try {
+			const data = await Get_Labels.run({page: Table1.pageNo, per_page: Table1.pageSize, search: Table1.searchText})
+			return data;
+		} catch (error) {
+			showAlert(error.message, "error")
+		}
+	},
+	async UpdateLabelPrice (label, amount, description) {
+		try{
+			const data = await Update_Label_Price.run({label, amount, description})
+			showAlert(`Succesfully updated ${data.label} details!`, "success")
+			await utils.GetLabels();
+			return data;
+		} catch (error) {
+			showAlert(error.message, "error")
+		}
+	}
+}
