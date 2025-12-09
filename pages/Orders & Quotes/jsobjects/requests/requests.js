dedf8429d3b66abff6694075f80ddf92101f674c
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2025-12-09 12:02:11 +0000

    Added extra fields to order items in list

--- pages/Orders & Quotes/jsobjects/requests/requests.js
+++ pages/Orders & Quotes/jsobjects/requests/requests.js
@@ -1,7 +1,14 @@
 export default {
 	async getAllOrders (group, search, status, customer, per_page, page) {
 		const orders = await List_All_Orders.run({group, search, status, customer, per_page, page})
+		const totalCount = List_All_Orders.responseMeta.headers['X-Total-Count'][0];
 		
-		return orders;
+		return { 
+			total_records: totalCount,
+			orders
+		};
+	},
+	test () {
+		requests.getAllOrders.data.order
 	}
 }
