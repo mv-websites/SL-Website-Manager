a0ec357c845371d7033e38901a45c4ab380416cc
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2025-12-16 13:16:43 +0000

    Built a new user form in the customers dashboard page

--- pages/Customers/jsobjects/JSObject1/JSObject1.js
+++ pages/Customers/jsobjects/JSObject1/JSObject1.js
@@ -1,19 +1,19 @@
 export default {
 	formatPowerBIFromMeta (meta_data) {
-    const item = meta_data.find(d => d.key === "reactive_jobs");
-    return item ? item : { "key": "reactive_jobs", "value": "" };
+		const item = meta_data.find(d => d.key === "reactive_jobs");
+		return item ? item : { "key": "reactive_jobs", "value": "" };
 	},
 	formatClientGroupFromMeta (meta_data) {
-    const item = meta_data.find(d => d.key === "client_group");
-    return item ? item : { "key": "client_group", "value": "" };
+		const item = meta_data.find(d => d.key === "client_group");
+		return item ? item : { "key": "client_group", "value": "" };
 	},
 	formatClientRefFromMeta (meta_data) {
-    const item = meta_data.find(d => d.key === "client_ref");
-    return item ? item : { "key": "client_ref", "value": "" };
+		const item = meta_data.find(d => d.key === "client_ref");
+		return item ? item : { "key": "client_ref", "value": "" };
 	},
 	async formattedCustomerData () {
 		const rawData = await Get_Customers.run({"search": Table1.searchText, "page": Table1.pageNo});
-		
+
 		const formattedData = rawData.map((customer) => {
 			return {
 				id: customer.id,
@@ -27,7 +27,37 @@ export default {
 				ClientRefObject: JSObject1.formatClientRefFromMeta(customer.meta_data)
 			}
 		})
-
 		return formattedData;
+	},
+	async saveNewUser(email, username, first_name, last_name = "") {
+		// Generate a random 3 word password
+		function randomWord(length = 5) {
+			const letters = "abcdefghijklmnopqrstuvwxyz";
+			return Array.from({ length }, () => letters[Math.floor(Math.random() * letters.length)]).join('');
+		}
+		function getRandomWords(count = 3) {
+			return Array.from({ length: count }, () => randomWord()).join('-');
+		}
+		const password = getRandomWords()
+
+		// Add customer
+		var newCustomer;
+		try {
+			newCustomer = await Create_a_Customer.run({email, first_name, last_name, username, password})
+			// Clear Form on success
+			resetWidget("first_name");
+			resetWidget("last_name");
+			resetWidget("username");
+			resetWidget("email_address");
+			showAlert("Succesfully created customer!", "success")
+		} catch (err) {
+			showAlert(err.message, "error")
+		}
+		
+		const customerObject = {
+			...newCustomer,
+			password
+		}
+		return customerObject;
 	}
 }
