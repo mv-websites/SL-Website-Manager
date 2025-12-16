1b6f9e8a33bbfd11d3f23ea001b5396a3aced713
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2025-12-16 16:56:49 +0000

    Created Edit order form

---
+++ pages/Edit Order or Quote/jsobjects/data/data.js
@@ -0,0 +1,32 @@
+export default {
+	async myFun2 () {
+		const order = await Retrieve_an_order.data;
+		// const orderObject = order.json
+
+		const essentialOrderInfo = {
+			id: order.id,
+			date_created: order.date_created,
+			editableData: {
+				status: order.status,
+				line_items: order.line_items.map((item) => ({
+					id: item.id,
+					name: item.name,
+					quantity: item.quantity,
+					meta_data: item.meta_data
+					.filter(
+						dataItem =>
+						typeof dataItem.value === 'string' &&
+						typeof dataItem.display_value === 'string'
+					)
+					.map(dataItem => ({
+						display_key: dataItem.display_key,
+						display_value: dataItem.display_value
+					}))
+				}))
+			},
+		}
+
+		return essentialOrderInfo
+
+	}
+}
---
+++ pages/Edit Order or Quote/jsobjects/utils/utils.js
@@ -0,0 +1,8 @@
+export default {
+	updateBilling () {
+		return billing_details.formData
+	},
+	updateShipping () {
+		return shipping_details.formData
+	}
+}
