fc6ce80662b87726960a44452c55ffb28ac217e9
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2026-01-09 13:57:23 +0000

    Completed implementation of selecting a different delivery method ont he order form

--- pages/Edit Order or Quote/jsobjects/utils/utils.js
+++ pages/Edit Order or Quote/jsobjects/utils/utils.js
@@ -19,7 +19,43 @@ export default {
 		const body = {
 			"id": order_id,
 			"line_items": utils.formatLineItems(),
+			"shipping_lines": [
+				{
+					"id": Retrieve_an_order.data.shipping_lines[0].id,
+					"method_title": Select1.selectedOptionLabel,
+					"instance_id": Select1.selectedOptionValue.toString(),
+					"total": Input1.text.toString()
+				}
+			],
 			"calculate_totals": true
+		}
+		try {
+			await Update_an_order.run({id: order_id, body: body});
+			showAlert("Updated succesfully!", "success")
+		} catch (err) {
+			showAlert("Update failed!", "error")
+		}
+		await data.retrieveAnOrderLineItems();
+		return body;
+	},
+	notesValue() {
+		var note; 
+		try {
+			note = Retrieve_an_order.data.meta_data.filter((item) => item.key === "_cms_note")[0].value
+		} catch {
+			note = ''
+		}
+		return note;
+	},
+	async saveNotes(order_id = Retrieve_an_order.data.id) {
+		const body = {
+			"id": order_id,
+			"meta_data": [
+				{
+					key: "_wcpdf_invoice_notes",
+					value: Notes.text
+				}
+			]
 		}
 		try {
 			await Update_an_order.run({id: order_id, body: body});
