f050e3df2ddd83a32b93fa9919044c90490ed34a
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2026-01-23 11:21:15 +0000

    Updated link to power bi report

---
+++ pages/Orders & Quotes/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,6 @@
+export default {
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
--- pages/Orders & Quotes/jsobjects/requests/requests.js
+++ pages/Orders & Quotes/jsobjects/requests/requests.js
@@ -33,5 +33,38 @@ export default {
 	},
 	test () {
 		requests.getAllOrders.data.order
+	},
+
+	async uploadMediaBinary() {
+		const myHeaders = {
+			"Content-Disposition": `attachment; filename=${FilePicker2.files[0].name}`,
+			"Content-Type": FilePicker2.files[0].type,
+			"Authorization": "Basic c2wtYXBpLXNlcnZpY2U6UnZQUyAzU1ZZIE5zQ2QgVVMzRiBQa0hDIGs5YmE="
+		};
+
+		const file = FilePicker2.files[0].data;
+		const base64 = file.split(",")[1];
+
+		const binaryString = atob(base64);
+		const bytes = new Uint8Array(binaryString.length);
+		for (let i = 0; i < binaryString.length; i++) {
+			bytes[i] = binaryString.charCodeAt(i);
+		}
+
+		const requestOptions = {
+			method: "POST",
+			headers: myHeaders,
+			body: bytes,
+			redirect: "follow"
+		};
+
+		const response = await fetch(
+			"https://www.service-line.co.uk/wp-json/wp/v2/media",
+			requestOptions
+		);
+
+		const text = await response.json();  // actual WordPress response
+		console.log("Upload response:", text);
+		return text;
 	}
 }
